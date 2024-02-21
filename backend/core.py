9f0d437442cbdb48c009309f6a247ed12ce1308b
SueGK <71711489+SueGK@users.noreply.github.com>
2024-02-21 14:53:59 +0000

    update code to new langchain version

--- backend/core.py
+++ backend/core.py
@@ -1,32 +1,57 @@
 import os
-from typing import Any, List, Dict
+from typing import Any, List, Dict, Set
 
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain.chains import ConversationalRetrievalChain
-from langchain.vectorstores import Pinecone
-import pinecone
-
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import Pinecone
 from consts import INDEX_NAME
 
-pinecone.init(
-    api_key=os.environ["PINECONE_API_KEY"],
-    environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
-)
 
+def run_llm(model: str, query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
+    """
+    A function that runs a conversational retrieval chain using OpenAI embeddings and Pinecone for document search.
 
-def run_llm(query: str, chat_history: List[Dict[str, Any]] = []) -> Any:
+    Args:
+        model (str): The model to be used for chat.
+        query (str): The question to be asked.
+        chat_history (List[Dict[str, Any]], optional): The chat history. Defaults to [].
+
+    Returns:
+        Any: The result of the conversational retrieval chain invocation.
+    """
     embeddings = OpenAIEmbeddings()
     docsearch = Pinecone.from_existing_index(
         index_name=INDEX_NAME, embedding=embeddings
     )
-    chat = ChatOpenAI(verbose=True, temperature=0)
+    chat = ChatOpenAI(verbose=True, model=model, temperature=0)
 
     qa = ConversationalRetrievalChain.from_llm(
         llm=chat, retriever=docsearch.as_retriever(), return_source_documents=True
     )
 
-    return qa({"question": query, "chat_history": chat_history})
+    return qa.invoke({"question": query, "chat_history": chat_history})
+
+
+def create_sources_string(source_urls: Set[str]) -> str:
+    """
+    Creates a string representation of a list of source URLs.
+
+    Args:
+        source_urls (Set[str]): A set of source URLs.
+
+    Returns:
+        str: A string representation of the list of source URLs.
+    """
+    if not source_urls:
+        return ""
+    sources_list = list(source_urls)
+    sources_list.sort()
+    sources_string = "sources:\n"
+    for i, source in enumerate(sources_list):
+        sources_string += f"{i+1}. {source}\n"
+    return sources_string
 
 
 if __name__ == "__main__":
